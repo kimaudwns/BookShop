f8e241649ce98feef618cbfe5c59133b1c0d97bd
kimaudwns <red6502001@naver.com>
2024-11-14 09:37:28 +0900

    Update kill_process.sh

--- deploy/scripts/kill_process.sh
+++ deploy/scripts/kill_process.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker-compose -f /home/ubuntu/deploy/scripts/docker-compose.yml down || true
+current_image="kimaudwns/bookshop:latest"
+build_image="kimaudwns/bookshop:${BUILD_NUMBER}"
 docker images --format "{{.Repository}}:{{.Tag}} {{.ID}}" | grep "kimaudwns/bookshop" | grep -v "${current_image}" | grep -v "${build_image}" | awk '{print $2}' | xargs -r docker rmi || true
